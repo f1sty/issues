603c92d563420e53810c42163e4ba99ff1365a6a
Yurii Skrynnykov <truef1s7@gmail.com>
2016-07-27 20:27:58 +0300

    Almost all done, except view. Code is little differ from Dave example

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -3,7 +3,29 @@ defmodule Issues.CLI do
   @default_count 4
 
   def run(argv) do
-    parse_args(argv)
+    argv |> parse_args |> process 
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response |> sort |> Enum.take(count)
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from GitHub: #{Map.get(error, "message", "unknown")}"
+    System.halt(2)
+  end
+
+  def sort(items) do
+    Enum.sort(items, fn a, b -> a["created_at"] <= b["created_at"] end)
   end
 
   def parse_args(argv) do
--- test/issues/cli_test.exs
+++ test/issues/cli_test.exs
@@ -1,6 +1,6 @@
 defmodule IssuesCLITest do
   use ExUnit.Case
-  import Issues.CLI, only: [parse_args: 1]
+  import Issues.CLI, only: [parse_args: 1, sort: 1]
   doctest Issues
 
   test ":help returned by parser given an -h or --help options" do
@@ -14,5 +14,15 @@ defmodule IssuesCLITest do
 
   test "two args given" do
     assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
+
+  test "testing sort order" do
+    result = sort(do_fake_created_at(["c", "a", "b"]))
+    issues = for item <- result, do: item["created_at"]
+    assert issues == ~w{a b c}
+  end
+
+  defp do_fake_created_at(values) do
+    for value <- values, do: %{"created_at" => value, "some_field" => "ffff"}
   end
 end
